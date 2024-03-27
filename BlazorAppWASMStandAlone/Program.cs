2470be7b9732b55073b43a51ed10fef823932e40
Fatming <osimejp@yahoo.co.jp>
2024-03-27 19:06:36 +0800

    add web request

--- BlazorAppWASMStandAlone/Program.cs
+++ BlazorAppWASMStandAlone/Program.cs
@@ -2,6 +2,7 @@ using BlazorAppWASMStandAlone;
 using BlazorSpinner;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Http;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<SpinnerService>();
+
+builder.Services.AddHttpClient();
 
 await builder.Build().RunAsync();

03208bb00e2a97f3c29d4369d14bf954cd93235a
Fatming <osimejp@yahoo.co.jp>
2024-04-08 16:48:17 +0800

    minor change

--- BlazorAppWASMStandAlone/ToPdf/ToPdf.cs
+++ BlazorAppWASMStandAlone/ToPdf/ToPdf.cs
@@ -1,5 +1,14 @@
 ﻿//https://www.c-sharpcorner.com/article/how-to-create-pdf-using-itextsharp-in-blazor/
 
+
+//https://stackoverflow.com/questions/69132556/how-to-download-edited-pdf-using-itextsharp-and-c-sharp-blazor
+
+//https://stackoverflow.com/questions/73345726/blazor-wasm-pdf-generation-with-itextsharp
+
+//https://taithienbo.medium.com/displaying-pdfs-in-blazor-ca6b5de2930c
+
+//https://ithelp.ithome.com.tw/articles/10312009?sc=rss.qu
+
 using iTextSharp.text;
 using iTextSharp.text.pdf;
 
@@ -17,8 +26,10 @@ namespace BlazorAppWASMStandAlone.ToPdf
             //Create a new document
             Document document = new Document();
 
+            FileStream l_FileStream = new FileStream(fileName, FileMode.Create);
+
             //Create a PDF writer
-            PdfWriter.GetInstance(document, new FileStream(fileName, FileMode.Create));
+            PdfWriter.GetInstance(document, l_FileStream);
 
             //Open the document
             document.Open();
@@ -27,16 +38,32 @@ namespace BlazorAppWASMStandAlone.ToPdf
             Font titleFont = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 18);
             Paragraph titleParagraph = new Paragraph(title, titleFont);
             titleParagraph.Alignment = Element.ALIGN_CENTER;
-            document.Add(titleParagraph);
+            bool l_bAdded = document.Add(titleParagraph);
 
             //Add some text
             Font bodyFont = FontFactory.GetFont(FontFactory.HELVETICA, 12);
             Paragraph bodyParagraph = new Paragraph(body, bodyFont);
             bodyParagraph.Alignment = Element.ALIGN_JUSTIFIED;
             bool l_bResult = document.Add(bodyParagraph);
+            Console.WriteLine("gen "+ fileName+ " result:" + l_bResult);
 
             //Close the document
             document.Close();
+
+
+            //https://stackoverflow.com/questions/76666812/download-a-memorystream-txt-file-from-a-blazor-page
+            //// Create the streams.
+            //MemoryStream destination = new MemoryStream();
+            //{
+
+            //    Console.WriteLine("Source length: {0}", l_FileStream.Length.ToString());
+
+            //    // Copy source to destination.
+            //    l_FileStream.CopyTo(destination);
+            //}
+
+            Console.WriteLine("Destination length: {0}", destination.Length.ToString());
+
         }
     }
 }

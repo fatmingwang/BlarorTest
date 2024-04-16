2d0fde98b3bc030f7328cae0e9699f358a2616eb
ElderWang <elder.wang@mail.diresoft.net>
2024-04-16 15:20:36 +0800

    add download pdf

--- BlazorAppWASMStandAlone/ToPdf/ToPdf.cs
+++ BlazorAppWASMStandAlone/ToPdf/ToPdf.cs
@@ -62,8 +62,42 @@ namespace BlazorAppWASMStandAlone.ToPdf
             //    l_FileStream.CopyTo(destination);
             //}
 
-            Console.WriteLine("Destination length: {0}", destination.Length.ToString());
+            //Console.WriteLine("Destination length: {0}", destination.Length.ToString());
 
+        }
+
+        public static Stream GetPDFStream(string title, string body)
+        {
+            byte[] l_PDFBytes;
+            //Create a new document
+            Document document = new Document();
+            MemoryStream l_FileStream = new MemoryStream();
+            //Create a PDF writer
+            PdfWriter.GetInstance(document, l_FileStream);
+
+            //Open the document
+            document.Open();
+
+            //Add a title
+            Font titleFont = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 18);
+            Paragraph titleParagraph = new Paragraph(title, titleFont);
+            titleParagraph.Alignment = Element.ALIGN_CENTER;
+            bool l_bAdded = document.Add(titleParagraph);
+
+            //Add some text
+            Font bodyFont = FontFactory.GetFont(FontFactory.HELVETICA, 12);
+            Paragraph bodyParagraph = new Paragraph(body, bodyFont);
+            bodyParagraph.Alignment = Element.ALIGN_JUSTIFIED;
+            bool l_bResult = document.Add(bodyParagraph);
+
+            //https://stackoverflow.com/questions/76666812/download-a-memorystream-txt-file-from-a-blazor-page
+            //Close the document
+            document.Close();
+
+            Console.WriteLine("Source length: {0}", l_FileStream.Length.ToString());
+            l_PDFBytes = l_FileStream.ToArray();
+            MemoryStream destination = new MemoryStream(l_PDFBytes);
+            return destination;
         }
     }
 }
